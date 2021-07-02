feec872058fcca37e6d9d8e4debb6c070acaeee2
Lucas Ondel <lucas.ondel@gmail.com>
2021-07-02 11:55:14 +0200

    Optimization + improved benchmark.

--- examples/benchmark.jl
+++ examples/benchmark.jl
@@ -6,12 +6,54 @@ using CUDA.CUSPARSE
 using BenchmarkTools
 using MarkovModels
 using SparseArrays
+using LinearAlgebra
+using LogExpFunctions
+
+BLAS.set_num_threads(1)
+
+#######################################################################
+# This is the "standard" way of implementing the forward-backward
+# algorithm (as would be done in python for example).
+
+function forward(Aᵀ, init, lhs::AbstractMatrix{T}) where T
+    N = size(lhs, 2)
+    log_α = fill!(similar(lhs), T(-Inf))
+    log_α[:, 1] = view(lhs, :, 1) + init
+    for n in 2:N
+        log_α[:, n] = lhs[:, n] + logsumexp(Aᵀ .+ reshape(log_α[:,n-1], 1, :),
+                                                     dims = 2)
+    end
+    log_α
+end
+
+function backward(A, final, lhs::AbstractMatrix{T}) where T
+    N = size(lhs, 2)
+    log_β = fill!(similar(lhs), T(-Inf))
+    log_β[:, end] = final
+    for n in N-1:-1:1
+        tmp = dropdims(logsumexp(A .+ reshape(log_β[:,n+1] .+ lhs[:, n+1], 1, :),
+                                 dims = 2), dims = 2)
+        log_β[:, n] = tmp
+    end
+    log_β
+end
+
+function forward_backward(A, Aᵀ, init, final, lhs)
+    log_α = forward(Aᵀ, init, lhs)
+    log_β = backward(A, final, lhs)
+    log_γ = log_α .+ log_β
+    sums = logsumexp(log_γ, dims = 1)
+    log_γ .- sums, minimum(sums)
+end
+
+#######################################################################
 
 function makefsm(SF, S)
     fsm = FSM{SF}()
 
     prev = addstate!(fsm, pdfindex = 1)
     setinit!(prev)
+    link!(fsm, prev, prev)
     for s in 2:S
         state = addstate!(fsm, pdfindex = s)
         link!(fsm, prev, state)
@@ -23,9 +65,9 @@ function makefsm(SF, S)
     fsm
 end
 
-function main(T, SF, S, N, pruning)
+function main(T, SF, S, N)
     # Generate some pseudo-(log)likelihood
-    lhs = convert(Matrix{SF}, randn(T, S, N))
+    lhs = convert(Matrix{SF}, zeros(T, S, N))
 
     # Build the Finite State Machine (FSM).
     fsm = makefsm(SF, S)
@@ -34,17 +76,36 @@ function main(T, SF, S, N, pruning)
     println("  float type: $T")
     println("  # states: $S")
     println("  # frames: $N")
-    println("  # pruning: $pruning")
     println()
+
+    println("αβrecursion (standard implementation) with dense CPU arrays:")
+    cfsm = compile(fsm, allocator = zeros)
+    Aᵀ = convert(Matrix{T}, cfsm.Aᵀ)
+    A = convert(Matrix{T}, cfsm.A)
+    init = convert(Vector{T}, cfsm.π)
+    final = convert(Vector{T}, cfsm.ω)
+    lhs_rs = convert(Matrix{T}, lhs)
+    @btime forward_backward($A, $Aᵀ, $init, $final, $lhs_rs)
+    println("------------------------------------------------")
 
     println("αβrecursion with dense CPU arrays:")
     cfsm = compile(fsm, allocator = zeros)
-    @btime αβrecursion($cfsm, $lhs, pruning = SF($pruning))
+    @btime αβrecursion($cfsm, $lhs)
     println("------------------------------------------------")
 
     println("αβrecursion with sparse CPU arrays:")
     cfsm = compile(fsm, allocator = spzeros)
-    @btime αβrecursion($cfsm, $lhs, pruning = SF($pruning))
+    @btime αβrecursion($cfsm, $lhs)
+    println("------------------------------------------------")
+
+    println("αβrecursion (standard implementation) with dense GPU arrays:")
+    cfsm = compile(fsm, allocator = zeros)
+    Aᵀ = convert(Matrix{T}, cfsm.Aᵀ) |> CuArray
+    A = convert(Matrix{T}, cfsm.A) |> CuArray
+    init = convert(Vector{T}, cfsm.π) |> CuArray
+    final = convert(Vector{T}, cfsm.ω) |> CuArray
+    lhs_rs = convert(Matrix{T}, lhs) |> CuArray
+    @btime forward_backward($A, $Aᵀ, $init, $final, $lhs_rs)
     println("------------------------------------------------")
 
     println("αβrecursion with dense GPU arrays:")
@@ -66,13 +127,9 @@ s = ArgParseSettings()
         arg_type = Int
         default = 1000
     "--num-states", "-S"
-        help = "number of observation frames"
+        help = "number of states"
         arg_type = Int
         default = 1000
-    "--pruning", "-p"
-        help = "pruning threshold"
-        arg_type = Float64
-        default = Inf
     "--single-precision"
         help = "use single precision"
         action = :store_true
@@ -80,9 +137,8 @@ end
 
 args = parse_args(s)
 
-T = args["single-precision"] ? Float32 : Float64
-SF = LogSemifield{T}
-S = args["num-states"]
-N = args["num-frames"]
-pruning = args["pruning"]
-main(T, SF, S, N, pruning)
+const T = args["single-precision"] ? Float32 : Float64
+const SF = LogSemifield{T}
+const S = args["num-states"]
+const N = args["num-frames"]
+main(T, SF, S, N)
--- src/algorithms.jl
+++ src/algorithms.jl
@@ -1,231 +1,98 @@
 # SPDX-License-Identifier: MIT
 
 """
-    αβrecursion(cfsm, lhs::AbstractMatrix{T}; pruning::T = typemax(T) where T
+    αβrecursion(cfsm, lhs::AbstractMatrix{T}) where T
 
 Return the states log conditional probabilities (as a matrix) and the
-total likelihood of the sequence. `pruning` is a `LogSemifield` number
-between `zero(T)` and `typemax(T)`. The lower it is the more pruning
-will occurs. Note that using `pruning` only makes sense if `cfsm`
-was compiled with `allocator = spzeros`.
+total likelihood of the sequence.
 
 See also: [`compile`](@ref)
 """
-function αβrecursion(cfsm, lhs::AbstractMatrix{T}; pruning::T = typemax(T)) where T
-    αβrecursion(cfsm.π, cfsm.ω, cfsm.A, cfsm.Aᵀ, lhs, pruning, cfsm.dists)
+function αβrecursion(cfsm, lhs::AbstractMatrix{T}) where T
+    αβrecursion(cfsm.π, cfsm.ω, cfsm.A, cfsm.Aᵀ, lhs)
 end
 
 #######################################################################
-# Pruning
-
-function remove_invalid_αpath!(αₙ::AbstractVector{T}, distances, N, n) where T
-    for i in 1:length(αₙ)
-        if distances[i] > (N - n) αₙ[i] = zero(T) end
-    end
-    αₙ
-end
-
-function remove_invalid_αpath!(αₙ::SparseVector{T}, distances, N, n) where T
-    I, V = findnz(αₙ)
-    for i in I
-        if distances[i] > (N - n) αₙ[i] = zero(T) end
-    end
-    αₙ
-end
-
-function prune_α!(αₙ::AbstractVector{T}, distances, N, n, threshold) where T
-    remove_invalid_αpath!(αₙ, distances, N, n)
-    maxval = maximum(αₙ)
-    for i in 1:length(αₙ)
-        αₙ[i] = (maxval/αₙ[i] ≤ threshold) ? αₙ[i] : zero(T)
-    end
-    αₙ
-end
-
-function prune_α!(αₙ::SparseVector{T}, distances, N, n, threshold) where T
-    remove_invalid_αpath!(αₙ, distances, N, n)
-    SparseArrays.fkeep!(αₙ, (i,v) -> maximum(αₙ)/v ≤ threshold)
-end
-
-function prune_β!(βₙ::AbstractVector{T}, n, threshold, α) where T
-    I = findall(α[:, n] .> zero(T))
-    maxval = maximum(βₙ)
-    for i in 1:length(βₙ)
-        βₙ[i] = i ∈ I && maxval/βₙ[i] ≤ threshold ? βₙ[i] : zero(T)
-    end
-    βₙ
-end
-
-function prune_β!(βₙ::SparseVector{T}, n, threshold, α) where T
-    I, V = findnz(α[:, n])
-    SparseArrays.fkeep!(βₙ, (i,v) -> i ∈ I && maximum(βₙ)/v ≤ threshold)
-end
-
-#######################################################################
-# Dense matrix implementation
+# Generic implementation
 
 function αrecursion!(α::AbstractMatrix{T}, π::AbstractVector{T},
-                     Aᵀ::AbstractMatrix{T}, lhs::AbstractMatrix,
-                     prune!::Function) where T
+                     Aᵀ::AbstractMatrix{T}, lhs::AbstractMatrix) where T
     N = size(lhs, 2)
+    buffer = similar(lhs[:, 1])
     α[:, 1] = lhs[:, 1] .* π
     for n in 2:N
         # Equivalent to:
         #αₙ = (Aᵀ * α[:,n-1]) .* lhs[:, n]
-        mul!(view(α, :, n), Aᵀ, view(α, :, n-1) .* view(lhs, :, n), one(T), zero(T))
-        prune!(view(α, :, n), n)
+
+        αₙ = view(α, :, n)
+        broadcast!(*, buffer, view(α, :, n-1), view(lhs, :, n))
+        mul!(αₙ, Aᵀ, buffer, one(T), zero(T))
     end
     α
 end
 
 function βrecursion!(β::AbstractMatrix{T}, ω::AbstractVector{T},
-                     A::AbstractMatrix{T}, lhs::AbstractMatrix{T},
-                     prune!::Function) where T
+                     A::AbstractMatrix{T}, lhs::AbstractMatrix{T}) where T
     N = size(lhs, 2)
+    buffer = similar(lhs[:, 1])
     β[:, end] = ω
     for n in N-1:-1:1
         # Equivalent to:
         #βₙ = A * (β[:, n+1] .* lhs[:, n+1])
-        mul!(view(β, :, n), A, view(β, :,n+1) .* view(lhs, :, n+1), one(T), zero(T))
-        prune!(view(β, :, n), n)
+
+        βₙ = view(β, :, n)
+        broadcast!(*, buffer, view(β, :, n+1), view(lhs, :, n+1))
+        mul!(βₙ, A, buffer, one(T), zero(T))
     end
     β
 end
 
 function αβrecursion(π::AbstractVector{T}, ω::AbstractVector{T},
                      A::AbstractMatrix{T}, Aᵀ::AbstractMatrix,
-                     lhs::AbstractMatrix{T},
-                     pruning, distances) where T
+                     lhs::AbstractMatrix{T}) where T
     S, N = length(π), size(lhs, 2)
     α = fill!(similar(lhs, T, S, N), zero(T))
     β = fill!(similar(lhs, T, S, N), zero(T))
+    γ = fill!(similar(lhs, T, S, N), zero(T))
 
-    pα = pruning == typemax(T) ? (αₙ, n) -> αₙ : (
-        (αₙ, n) -> prune_α!(αₙ, distances, N, n, pruning))
-    pβ = pruning == typemax(T) ? (βₙ, n) -> βₙ : (
-        (βₙ, n) -> prune_β!(βₙ, n, pruning, α))
+    αrecursion!(α, π, Aᵀ, lhs)
+    βrecursion!(β, ω, A, lhs)
 
-    αrecursion!(α, π, Aᵀ, lhs, pα)
-    βrecursion!(β, ω, A, lhs, pβ)
+    broadcast!(*, γ, α, β)
+    sums = sum(γ, dims = 1)
+    broadcast!(/, γ, γ, sums)
 
-    αβ = α .* β
-    sums = sum(αβ, dims = 1)
-    αβ ./ sums, maximum(sums)
-end
-
-#######################################################################
-# Sparse matrix implementation
-
-function αrecursion!(α::AbstractSparseMatrix{T}, π::AbstractSparseVector{T},
-                     Aᵀ::AbstractSparseMatrix{T}, lhs::AbstractMatrix{T},
-                     prune!::Function) where T
-    N = size(lhs, 2)
-    α[:, 1] = lhs[:,1] .* π
-    for n in 2:N
-        # Equivalent to:
-        #αₙ = (Aᵀ * α[:,n-1]) .* lhs[:, n]
-        αₙ = mul!(similar(α[:,n], T, size(Aᵀ, 1)), Aᵀ,
-                  α[:,n-1] .* lhs[:,n], one(T), zero(T))
-        prune!(αₙ, n)
-        α[:, n] = αₙ
-    end
-    α
-end
-
-function βrecursion!(β::AbstractSparseMatrix{T}, ω::AbstractSparseVector{T},
-                     A::AbstractSparseMatrix{T}, lhs::AbstractMatrix{T},
-                     prune!::Function) where T
-    N = size(lhs, 2)
-    β[:, end] = ω
-    for n in N-1:-1:1
-        # Equivalent to:
-        #βₙ = A * (β[:, n+1] .* lhs[:, n+1])
-        βₙ = mul!(similar(β[:, n], T, size(A, 2)), A,
-                  β[:,n+1] .* lhs[:, n+1], one(T), zero(T))
-        prune!(βₙ, n)
-        β[:, n] = βₙ
-    end
-    β
-end
-
-function normalize_spmatrix(M::AbstractSparseMatrix{T}) where T
-    sums = sum(M, dims = 1)
-    I, J, V = findnz(M)
-    for i in 1:length(V)
-        V[i] /= sums[J[i]]
-    end
-    sparse(I, J, V), maximum(sums)
-end
-
-function αβrecursion(π::AbstractVector{T}, ω::AbstractVector{T},
-                     A::AbstractSparseMatrix{T}, Aᵀ::AbstractSparseMatrix{T},
-                     lhs::AbstractMatrix{T}, pruning, distances) where T
-    S, N = length(π), size(lhs, 2)
-    α = spzeros(T, S, N)
-    β = spzeros(T, S, N)
-
-    pα = pruning == typemax(T) ? (αₙ, n) -> αₙ : (
-        (αₙ, n) -> prune_α!(αₙ, distances, N, n, pruning))
-    pβ = (βₙ, n) -> prune_β!(βₙ, n, pruning, α)
-
-    αrecursion!(α, π, Aᵀ, lhs, pα)
-    βrecursion!(β, ω, A, lhs, pβ)
-
-    normalize_spmatrix(α .* β)
+    γ, minimum(sums)
 end
 
 #######################################################################
 # Sparse GPU matrix implementation
 
 function αrecursion!(α::CuMatrix{T}, π::CuSparseVector{T},
-                     Aᵀ::CuSparseMatrixCSR{T}, lhs::CuMatrix{T},
-                     prune!::Function) where T
+                     Aᵀ::CuSparseMatrixCSR{T}, lhs::CuMatrix{T}) where T
     N = size(lhs, 2)
     buffer = similar(α[:,1], T, length(π))
-    #α[:, 1] = lhs[:, 1] .* π
     elmul_svdv!(view(α, :, 1), π, view(lhs, :, 1))
     for n in 2:N
         # Equivalent to:
         #αₙ = (Aᵀ * α[:,n-1]) .* lhs[:, n]
-        #αₙ = mul!(buffer, Aᵀ, view(α, :, n-1) .* view(lhs, :, n))
+
         mul_smdv!(view(α, :, n), Aᵀ, view(α, :, n-1) .* view(lhs, :, n))
-        prune!(view(α, :, n), n)
     end
     α
 end
 
 function βrecursion!(β::CuMatrix{T}, ω::CuSparseVector{T},
-                     A::CuSparseMatrixCSR{T}, lhs::CuArray{T},
-                     prune!::Function) where T
+                     A::CuSparseMatrixCSR{T}, lhs::CuArray{T}) where T
     N = size(lhs, 2)
     β[:, end] = ω
     for n in N-1:-1:1
         # Equivalent to:
         #βₙ = A * (β[:, n+1] .* lhs[:, n+1])
         #βₙ = mul!(similar(β[:, n], T, size(A, 2)), A, β[:,n+1] .* lhs[:, n+1])
+
         mul_smdv!(view(β, :, n), A, view(β, :, n+1) .* view(lhs, :, n+1))
-        prune!(view(β, :, n), n)
     end
     β
-end
-
-function αβrecursion(π::CuSparseVector{T}, ω::CuSparseVector{T},
-                     A::CuSparseMatrixCSR{T}, Aᵀ::CuSparseMatrixCSR{T},
-                     lhs::CuArray{T}, pruning, distances) where T
-    S, N = length(π), size(lhs, 2)
-    α = fill!(similar(lhs, T, S, N), zero(T))
-    β = fill!(similar(lhs, T, S, N), zero(T))
-
-    pα = pruning == typemax(T) ? (αₙ, n) -> αₙ : (
-        (αₙ, n) -> prune_α!(αₙ, distances, N, n, pruning))
-    pβ = pruning == typemax(T) ? (βₙ, n) -> βₙ : (
-        (βₙ, n) -> prune_β!(βₙ, n, pruning, α))
-
-    αrecursion!(α, π, Aᵀ, lhs, pα)
-    βrecursion!(β, ω, A, lhs, pβ)
-
-    αβ = α .* β
-    sums = sum(αβ, dims = 1)
-    αβ ./ sums, maximum(sums)
 end
 
