1da0caf2e1ce24a53f6864264f2aaee423b04104
Lucas Ondel <lucas.ondel@gmail.com>
2021-07-19 19:12:36 +0200

    initialize output memory when performing sparse matrix multiplication

--- src/linalg.jl
+++ src/linalg.jl
@@ -10,13 +10,21 @@ semifield algebra operations.
 eldiv!(out, x::AbstractArray, y::AbstractArray) = broadcast!(/, out, x, y)
 
 # Element-wise multiplication.
-elmul!(out, x::AbstractArray, y::AbstractArray) = broadcast!(*, out, x, y)
+elmul!(out, x::AbstractArray, y::AbstractArray) =  broadcast!(*, out, x, y)
+function elmul!(out, x::AbstractSparseArray{T}, y::AbstractArray{T}) where T
+    fill!(out, zero(T))
+    broadcast!(*, out, x, y)
+end
 elmul!(out, x::CuSparseVector, y::AbstractVector) = elmul_svdv!(out, x, y)
 elmul!(out, x::CuSparseVector, Y::AbstractMatrix) = elmul_svdm!(out, x, Y)
 
 # Matrix multiplication.
 matmul!(out, X::AbstractMatrix{T}, y::AbstractArray{T}) where T =
     mul!(out, X, y, one(T), zero(T))
+function matmul!(out, X::AbstractSparseMatrix{T}, y::AbstractArray{T}) where T
+    fill!(out, zero(T))
+    mul!(out, X, y, one(T), zero(T))
+end
 matmul!(out, X::CuMatrix{T}, Y::CuMatrix{T}) where T =
     mul_dmdm!(out, X, Y)
 matmul!(out, X::CuSparseMatrixCSR{T}, y::AbstractVector{T}) where T =
