dd959e07c7d176956cc4f153f79c849066db0e11
Lucas Ondel <lucas.ondel@gmail.com>
2021-08-03 10:27:47 +0200

    remove unnecessary promote rule

--- src/semifields.jl
+++ src/semifields.jl
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: MIT
 
-# Stable implemenation of the log(exp(x) + exp(y)).
+# Stable implementation of the log(exp(x) + exp(y)).
 function logaddexp(x::T, y::T) where T
     diff = zero(T)
     if x < y
@@ -20,7 +20,7 @@ abstract type Semifield <: Number end
 
 Base.convert(T::Type{<:Number}, x::Semifield) = T(x.val)
 Base.promote(x::SF, y::Real) where SF <: Semifield = x, SF(y)
-Base.promote(x::Real, y::SF) where SF <: Semifield = SF(x), y
+#Base.promote(x::Real, y::SF) where SF <: Semifield = SF(x), y
 Base.show(io::IO, x::Semifield) = print(io, x.val)
 
 #======================================================================
