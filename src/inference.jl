76125aa63d0b53aea0f7c690052970d80795654d
Lucas Ondel <lucas.ondel@gmail.com>
2022-07-24 04:26:12 +0200

    fixed: batch with 2 compiled fsas

--- src/inference.jl
+++ src/inference.jl
@@ -33,9 +33,9 @@ final state won't be merge together and the resulting FSA will have
 several "virtual" final state. The output of `batch` should be
 considered as several independent FSAs packed in a single structure.
 """
-batch(fsa1::CompiledFSA{K}, fsa2::FSA{K}) where K =
+batch(fsa1::CompiledFSA{K}, fsa2::CompiledFSA{K}) where K =
     CompiledFSA(vcat(fsa1.α̂, fsa2.α̂), blockdiag(fsa1.T̂, fsa2.T̂))
-batch(fsa1::CompiledFSA{K}, fsas::FSA{K}...) where K =
+batch(fsa1::CompiledFSA{K}, fsas::CompiledFSA{K}...) where K =
     foldl(batch, fsas, init = fsa1)
 
 """
