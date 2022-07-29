46215f8348cd4fc60f05756f496728243ad429cf
Lucas Ondel <lucas.ondel@gmail.com>
2022-07-29 18:20:00 -0400

    added LIL matrix

--- src/linalg.jl
+++ src/linalg.jl
@@ -119,6 +119,27 @@ function Base.vcat(A::SparseLowRankMatrix{K,TS,TU,TV},
     )
 end
 
+#=====================================================================
+LIL (LIst of List) matrix format.
+======================================================================#
+
+struct LILMatrix{K, Tv <: AbstractVector{K}} <: AbstractMatrix{K}
+    numrows::Int64
+    cols::Vector{Tv}
+end
+
+Base.size(M::LILMatrix) = (M.numrows, length(M.cols))
+
+Base.getindex(M::LILMatrix, i::Int, j::Int) = M.cols[j][i]
+Base.getindex(M::LILMatrix, i::IndexRange, j::Int) = M.cols[j]
+Base.setindex!(M::LILMatrix, v, i::Int, j::Int) = M.cols[j][i] = v
+Base.setindex!(M::LILMatrix, v, i::IndexRange, j::Int) = M.cols[j] = v
+
+function lilzeros(K::Type{<:Any}, numrows::Int, numcols::Int)
+    LILMatrix(numrows, repeat([spzeros(K, numrows)], numcols))
+end
+lilzeros(numrows, numcols) = lilzeros(Float64, numrows, numcols)
+
 #======================================================================
 Conversion from/to CSR/CSC matrix with semiring elements.
 In all cases, it is necessary to convert the underlying data to
